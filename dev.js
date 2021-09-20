dbb1f9f92ca9450cfcc884933e8c6f767b0cf3ea
folkforms <alan.bergin@gmail.com>
2021-09-20 23:06:31 +0100

    Fail gracefully when node not found

--- dev.js
+++ dev.js
@@ -1,8 +1,15 @@
 const tree = require("./tree");
 
 const dev = (data, treeSearch, handler, options) => {
+  const treeSearchOriginal = treeSearch.join(" ");
+
   // Get the target node
   const node = tree.find(data, treeSearch);
+  if(!node) {
+    console.error(`Error: Could not find ${JSON.stringify(treeSearchOriginal)}`);
+    // FIXME Print list as deep as we could find
+    return { code: 1 };
+  }
 
   // Run the command
   const code = handler(node, options);
--- tree.js
+++ tree.js
@@ -3,6 +3,9 @@ const find = (data, treeSearch) => {
   let node = data["data"]; // Should match the first data node
   while(treeSearch.length > 0) {
     const term = treeSearch.splice(0, 1);
+    if(!node) {
+      return null;
+    }
     node = node[term];
   }
   return node;
