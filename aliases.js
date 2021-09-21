5b36c51d6beac01277418cf5882756d5253ced90
folkforms <alan.bergin@gmail.com>
2021-09-21 20:21:29 +0100

    Skip alias code if no aliases defined

--- aliases.js
+++ aliases.js
@@ -1,8 +1,11 @@
 const tree = require("./tree");
 
 const aliases = (aliasList, treeSearch) => {
-  const keys = Object.keys(aliasList);
+  if(!aliasList) {
+    return treeSearch;
+  }
 
+  const keys = Object.keys(aliasList);
   treeSearch = treeSearch.map(item => {
     for(let i = 0; i < keys.length; i++) {
       const key = keys[i];
