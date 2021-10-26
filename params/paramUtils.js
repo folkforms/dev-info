318a3fc0dba292f277e1683649f13beb895d02b6
folkforms <alan.bergin@gmail.com>
2021-10-26 19:12:38 +0100

    Update code that gets project name

--- params/paramUtils.js
+++ params/paramUtils.js
@@ -3,19 +3,16 @@ const fs = require("fs-extra");
 const yaml = require("js-yaml");
 
 const getFilenameForParsing = () => {
-  let file1 = fileio.glob("hubspot.deploy/*.yaml");
+  let file1 = fileio.glob("hubspot.deploy/*.yaml").sort((a, b) => a.length - b.length);
   let file2 = fileio.glob("hubspot.deploy/*All.yaml");
   let file3 = fileio.glob("hubspot.deploy/*Api.yaml");
-  const found = file1.length === 1 || file2.length === 1 || file3.length === 1;
-  if(!found) {
-    throw new Error("Could not find hubspot.deploy/*.yaml file");
-  }
-  if(file1.length === 1) {
-    return file1[0];
-  } else if(file2.length === 1) {
-    return file2[0];
+  let r;
+  if(file2.length === 1) {
+    r = file2[0];
   } else if(file3.length === 1) {
-    return file3[0];
+    r = file3[0];
+  } else if(file1.length > 0) {
+    r = file1[0];
   } else {
     const errorMessage = `Error getting hubspot.deploy/*.yaml file `
       + `(file1=${JSON.stringify(file1)}, `
@@ -23,6 +20,7 @@ const getFilenameForParsing = () => {
       + `file3=${JSON.stringify(file3)})`;
     throw new Error(errorMessage);
   }
+  return r;
 }
 
 const getYaml = filename => {
